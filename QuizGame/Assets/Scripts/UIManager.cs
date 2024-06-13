5849062b3bb54eadf2377cee13b42a8bc837deb8
ixa532 <belobelle532@gmail.com>
2024-06-13 14:11:30 -0300

    Adicionado
    Difficult, theme, game over....

--- QuizGame/Assets/Scripts/UIManager.cs
+++ QuizGame/Assets/Scripts/UIManager.cs
@@ -27,11 +27,11 @@ public class UIManager : MonoBehaviour
         startButton.onClick.AddListener(() => GameManager.instance.StartGame(dificultyDropDown.value, themeDropdown.value));
         nextButton.onClick.AddListener(() => QuizManager.instance.SelectQuiz(GameManager.instance.Theme, GameManager.instance.Dificulty));
 
-        for (int i = 0; i < answersButtons.Lenght; i++) ;
+        for (int i = 0; i < answersButtons.Length; i++)
         {
             int x = i;
             answersButtons[i].onClick.AddListener(() => QuizManager.instance.CheckAnswer(x));
-            answersButtons[i].onClick.Listener(() => nestButton.interactable = true);
+            answersButtons[i].onClick.AddListener(() => nextButton.interactable = true);
         }
     }
         
@@ -41,11 +41,26 @@ public class UIManager : MonoBehaviour
         for(int i = 0; i < answersButtons.Length; i++)
         {
             answersButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = quizSelected.Answers[i]; // iterador é o controlador do for
+            answersButtons[i].interactable = true;
             answersButtons[i].GetComponent<Image>().color = Color.white;
         }
-        
+        nextButton.interactable = false;
     }
 
+    public void HighlightButton(int correctAnswer, int answerSelected)
+    {
+        answersButtons[correctAnswer].GetComponent<Image>().color = Color.green;
+
+        if (answerSelected != correctAnswer)
+        {
+            answersButtons[answerSelected].GetComponent<Image>().color = Color.red;
+        }
+
+        for (int i = 0; i < answersButtons.Length; i++)       
+        {
+            answersButtons[i].interactable = false;
+        }
+    }
 
     public void SetMenu(bool active)
     { 
